e0b70a328b006d596d55518d74f9088dc1abf948
Sathwik <Sathwik@host.docker.internal>
2021-07-22 20:00:02 +0530

    2

--- Hackathon_BookMyShow/src/main/java/com/bookmyshow/baseclass/BaseTest.java
+++ Hackathon_BookMyShow/src/main/java/com/bookmyshow/baseclass/BaseTest.java
@@ -3,17 +3,16 @@ package com.bookmyshow.baseclass;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -36,55 +35,55 @@ public class BaseTest {
 	public void invokeBrowser(String browserName) {
 		// Select and browser based on the browserName
 
-		try {
-			if (browserName.equalsIgnoreCase("Chrome")) {
-				ChromeOptions options = new ChromeOptions();
-				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
-						"--homepage=about:blank", "--no-first-run"));
-				options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
-				options.addArguments("--disable-notifications");
-				DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-				capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-				driver = new RemoteWebDriver(new  URL("http://192.168.0.111:4444/wd/hub"),capabilities);
-				
-			} else if (browserName.equalsIgnoreCase("Mozilla")) {
-				FirefoxOptions options = new FirefoxOptions();
-				options.setProfile(new FirefoxProfile());
-				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
-						"--homepage=about:blank", "--no-first-run"));
-				options.addPreference("dom.webnotifications.enabled", false);
-				DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-				capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
-				driver = new RemoteWebDriver(new  URL("http://192.168.0.111:4444/wd/hub"),capabilities);
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-
 //		try {
 //			if (browserName.equalsIgnoreCase("Chrome")) {
-//				System.setProperty("webdriver.chrome.driver",
-//						System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
 //				ChromeOptions options = new ChromeOptions();
 //				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
 //						"--homepage=about:blank", "--no-first-run"));
 //				options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
 //				options.addArguments("--disable-notifications");
-//				driver = new ChromeDriver(options);
+//				DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//				capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//				driver = new RemoteWebDriver(new  URL("http://192.168.0.111:4444/wd/hub"),capabilities);
 //				
 //			} else if (browserName.equalsIgnoreCase("Mozilla")) {
-//				System.setProperty("webdriver.gecko.driver",
-//						System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
 //				FirefoxOptions options = new FirefoxOptions();
 //				options.setProfile(new FirefoxProfile());
 //				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
 //						"--homepage=about:blank", "--no-first-run"));
 //				options.addPreference("dom.webnotifications.enabled", false);
-//				driver = new FirefoxDriver(options);
+//				DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//				capabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options);
+//				driver = new RemoteWebDriver(new  URL("http://192.168.0.111:4444/wd/hub"),capabilities);
 //			}
 //		} catch (Exception e) {
 //			System.out.println(e.getMessage());
 //		}
+
+		try {
+			if (browserName.equalsIgnoreCase("Chrome")) {
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
+						"--homepage=about:blank", "--no-first-run"));
+				options.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				options.addArguments("--disable-notifications");
+				driver = new ChromeDriver(options);
+				
+			} else if (browserName.equalsIgnoreCase("Mozilla")) {
+				System.setProperty("webdriver.gecko.driver",
+						System.getProperty("user.dir") + "\\drivers\\geckodriver.exe");
+				FirefoxOptions options = new FirefoxOptions();
+				options.setProfile(new FirefoxProfile());
+				options.addArguments(Arrays.asList("--no-sandbox", "--ignore-certificate-errors",
+						"--homepage=about:blank", "--no-first-run"));
+				options.addPreference("dom.webnotifications.enabled", false);
+				driver = new FirefoxDriver(options);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		
 		driver.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
